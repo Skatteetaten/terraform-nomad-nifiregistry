e10911086ec5173934a8df3472f3d003a05ef50b
Cherradi <m77773@u1804i-0473.SKEAD.NO>
2020-12-04 11:36:46 +0100

    Kjørte terraform fmt -recursive for formatering

--- main.tf
+++ main.tf
@@ -5,19 +5,19 @@ locals {
 data "template_file" "nomad_job_nifi_registry" {
   template = file("${path.module}/nomad/nifi_registry.hcl")
   vars = {
-    datacenters              = local.datacenters
-    namespace                = var.nomad_namespace
-    host_volume              = var.nomad_host_volume
-    image                    = var.container_image
-    service_name             = var.service_name
-    host                     = var.host
-    port                     = var.port
-    cpu                      = var.resource.cpu
-    memory                   = var.resource.memory
-    cpu_proxy                = var.resource_proxy.cpu
-    memory_proxy             = var.resource_proxy.memory
-    use_host_volume          = var.use_host_volume
-    use_canary               = var.use_canary
+    datacenters     = local.datacenters
+    namespace       = var.nomad_namespace
+    host_volume     = var.nomad_host_volume
+    image           = var.container_image
+    service_name    = var.service_name
+    host            = var.host
+    port            = var.port
+    cpu             = var.resource.cpu
+    memory          = var.resource.memory
+    cpu_proxy       = var.resource_proxy.cpu
+    memory_proxy    = var.resource_proxy.memory
+    use_host_volume = var.use_host_volume
+    use_canary      = var.use_canary
   }
 }
 
