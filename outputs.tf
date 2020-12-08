86ae93d5cc32ef9b6bef0bba844a60480146ce93
Karima Cherradi <50236194+karimacherradi@users.noreply.github.com>
2020-12-08 07:23:43 +0100

    Lagt til port og host til output.tf (#15)
    * Lagt til port og host til output.tf
    
    * Oppdatert README og CHANGELOG
    
    * updated .gitignore file
    
    * roll back to master-version
    
    * update .gitignore
    
    Co-authored-by: Cherradi <m77773@u1804i-0473.SKEAD.NO>
    Co-authored-by: Hanne Maria Vister <HanneMaria.Vister@skatteetaten.no>

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "nifi_reg_service_name" {
   description = "Nifi registry service name"
   value       = data.template_file.nomad_job_nifi_registry.vars.service_name
 }
+
+output "nifi_reg_port" {
+  description = "Nifi registry port number"
+  value       = data.template_file.nomad_job_nifi_registry.vars.port
+}
+
+output "nifi_reg_host" {
+  description = "Nifi registry host"
+  value       = data.template_file.nomad_job_nifi_registry.vars.host
+}
